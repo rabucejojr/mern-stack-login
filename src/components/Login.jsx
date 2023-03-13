6eae334f2ebe8f75f39ab85f7c7a2eacd16eecf1
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-13 17:08:16 +0800

    modified components

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { TextField, Button, Card } from '@mui/material'
+const Login = () => {
+    return (
+        <>
+            <Card >
+                <TextField label="Username" variant="outlined" type='text' fullWidth />
+                <TextField label="Password" variant="outlined" type='password' fullWidth />
+                <Button variant='contained' fullWidth>Login</Button>
+            </Card>
+        </>
+    )
+}
+
+export default Login
