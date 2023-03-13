d41e14c3e938f80fc06e4a06e522274e783c2543
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-13 15:28:08 +0800

    deleted app.js contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,9 @@
-import './App.css';
-import Homepage from "./components/homepage/Homepage"
-import Login from "./components/login/Login"
-import Register from "./components/register/Register"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
+import React from 'react'
 
-} from "react-router-dom";
-import { useState } from 'react';
-function App() {
-  const [user, setLoginUser] = useState({
-
-  })
+const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={Homepage}/>
-          <Route path="/Login" element={<Login/>}/>
-          <Route path="/Register" element={<Register />}/>
-        </Routes>
-
-      </Router>
-
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
