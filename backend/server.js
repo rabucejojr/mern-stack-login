d2e2e053ed086a1818996c92f45e7504c4055031
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-14 09:50:00 +0800

    added configurations for backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+
+const app = express()
+
+app.get('/api/login', (req, res) => {
+    res.send('Login successful')
+})
+
+app.listen(port, () => console.log(`server started on port ${port}`))
