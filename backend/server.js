aa7ecda48bf1bfe1133d21352db5000e1b44e1a3
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-14 12:04:08 +0800

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,5 @@ const app = express()
 //calls routes
 app.use('/api/crud', require('./routes/appRoutes'))
 
-
 //prints server ports
 app.listen(port, () => console.log(`server started on port ${port}`))
