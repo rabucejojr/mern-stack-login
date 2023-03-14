9c11e8140b431bf158cfa450c588d348fec93c40
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-14 11:44:49 +0800

    modified for crud application

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,7 @@ const port = process.env.port || 5000
 const app = express()
 
 //calls routes
-app.use('/api/', require('./routes/appRoutes'))
-app.use('/api/read', require('./routes/appRoutes'))
-app.use('/api/update', require('./routes/appRoutes'))
-app.use('/api/delete', require('./routes/appRoutes'))
+app.use('/api/crud', require('./routes/appRoutes'))
 
 
 //prints server ports
