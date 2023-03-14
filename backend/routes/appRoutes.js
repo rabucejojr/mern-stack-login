05872a8ed9d46612f0fdc7076d29e0d81b1f7f4a
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-14 10:42:59 +0800

    added comments for documentation

--- backend/routes/appRoutes.js
+++ backend/routes/appRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
+
+
+// routes
 router.get('/', (req, res) => {
     res.status(200).json({ message: 'Login successful' })
 })
-
 router.post('/', (req, res) => {
     res.status(200).json({ message: 'Post successful' })
 })
-
 router.put('/:id', (req, res) => {
     res.status(200).json({ message: `Update successful ${req.params.id}` })
 })
-
 router.delete('/:id', (req, res) => {
     res.status(200).json({ message: `Delete successful ${req.params.id}` })
 })
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const port = process.env.port || 5000
 
 const app = express()
 
+//calls routes
 app.use('/api/login', require('./routes/appRoutes'))
 app.use('/api/post', require('./routes/appRoutes'))
 app.use('/api/update', require('./routes/appRoutes'))
 app.use('/api/delete', require('./routes/appRoutes'))
 
+
+//prints server ports
 app.listen(port, () => console.log(`server started on port ${port}`))
