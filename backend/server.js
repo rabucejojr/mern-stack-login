7db7517b547d9d73fdb62c35df74eaa548d4c581
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-14 10:29:13 +0800

    added crud in api backend

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,8 @@ const port = process.env.port || 5000
 const app = express()
 
 app.use('/api/login', require('./routes/appRoutes'))
+app.use('/api/post', require('./routes/appRoutes'))
+app.use('/api/update', require('./routes/appRoutes'))
+app.use('/api/delete', require('./routes/appRoutes'))
 
 app.listen(port, () => console.log(`server started on port ${port}`))
